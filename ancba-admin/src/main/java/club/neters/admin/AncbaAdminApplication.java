e28cf0a9a7e2d3beb7da098d1eafc442a28bff88
anjoy8 <3143422472@qq.com>
2021-06-25 11:41:01 +0800

    feat：spring boot admin module

---
+++ ancba-admin/src/main/java/club/neters/admin/AncbaAdminApplication.java
@@ -0,0 +1,13 @@
+package club.neters.admin;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableAdminServer
+public class AncbaAdminApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AncbaAdminApplication.class, args);
+    }
+}
---
+++ ancba-user/src/main/java/club/neters/user/api/controller/UserController.java
@@ -0,0 +1,13 @@
+package club.neters.user.api.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+    @GetMapping("/")
+    public String test(){
+        return "xx";
+    }
+}
