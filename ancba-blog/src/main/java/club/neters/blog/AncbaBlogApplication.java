051b5f79f3e291c72b72af1a260355e9a63e3f4d
anjoy8 <3143422472@qq.com>
2021-06-25 13:58:01 +0800

    fix：rename ancba-blog

--- ancba-blog/src/main/java/club/neters/blog/AncbaBlogApplication.java
+++ ancba-blog/src/main/java/club/neters/blog/AncbaBlogApplication.java
@@ -6,10 +6,10 @@ import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {"club.neters"})//扫描其他模块的配置类，有club.neters路径相同都可以
-public class BlogSpringApplication {
+public class AncbaBlogApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(BlogSpringApplication.class, args);
+        SpringApplication.run(AncbaBlogApplication.class, args);
         System.out.println("=================项目启动成功=============");
     }
 
