771417bc2c8c8e0ba7a4325b458569f045c06bd9
wuare <wuareb@gmail.com>
2021-06-25 09:35:19 +0800

    Add user module

---
+++ ancba-user/src/main/java/club/neters/user/AncbaUserApplication.java
@@ -0,0 +1,16 @@
+package club.neters.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author wuare
+ * @date 2021/6/25
+ */
+@SpringBootApplication
+public class AncbaUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AncbaUserApplication.class, args);
+    }
+}
