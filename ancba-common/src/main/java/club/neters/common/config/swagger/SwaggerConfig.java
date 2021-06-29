0ed8eb479a36f72373e8be35c9aa577699490ee3
wuare <wuareb@gmail.com>
2021-06-29 16:28:21 +0800

    Update SwaggerConfig.java

--- ancba-common/src/main/java/club/neters/common/config/swagger/SwaggerConfig.java
+++ ancba-common/src/main/java/club/neters/common/config/swagger/SwaggerConfig.java
@@ -1,9 +1,9 @@
 package club.neters.common.config.swagger;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -11,14 +11,15 @@ import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import springfox.documentation.swagger2.configuration.Swagger2DocumentationConfiguration;
 
 /**
  * @author wuare
  * @date 2021/6/29
  */
+@ConditionalOnBean(name = "swaggerConfigProperties")
 @Configuration
-@EnableSwagger2
+@Import(Swagger2DocumentationConfiguration.class)
 public class SwaggerConfig {
 
     private SwaggerConfigProperties swaggerConfigProperties;
@@ -42,9 +43,9 @@ public class SwaggerConfig {
                 .build();
     }
 
-    @Autowired
-    @Qualifier("swaggerConfigProperties")
     public void setSwaggerConfigProperties(SwaggerConfigProperties swaggerConfigProperties) {
         this.swaggerConfigProperties = swaggerConfigProperties;
     }
 }
+
+
