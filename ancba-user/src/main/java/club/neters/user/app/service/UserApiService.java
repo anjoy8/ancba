bd131b683a9803ec35386fd2bb14846bef29ff95
anjoy8 <3143422472@qq.com>
2021-07-02 19:13:44 +0800

    feat：feign access security api

--- ancba-user/src/main/java/club/neters/user/app/service/UserApiService.java
+++ ancba-user/src/main/java/club/neters/user/app/service/UserApiService.java
@@ -1,5 +1,6 @@
 package club.neters.user.app.service;
 
+import club.neters.user.core.config.feign.FeignConfig;
 import club.neters.user.domain.request.UserInfoRequestFromBlog;
 import club.neters.user.domain.vo.ApiResultVo;
 import club.neters.user.domain.vo.UserInfoVoFromBlog;
@@ -9,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 import java.util.List;
 
-@FeignClient("ancba-blog")
+@FeignClient(value = "ancba-blog", configuration = {FeignConfig.class})
 public interface UserApiService {
     /**
      * 查询用户数据
---
+++ ancba-user/src/main/java/club/neters/user/core/config/feign/FeignConfig.java
@@ -0,0 +1,20 @@
+package club.neters.user.core.config.feign;
+
+import feign.RequestInterceptor;
+import feign.RequestTemplate;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Configuration
+public class FeignConfig implements RequestInterceptor {
+    @Override
+    public void apply(RequestTemplate requestTemplate) {
+        ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        HttpServletRequest request = attributes.getRequest();
+        requestTemplate.header(HttpHeaders.AUTHORIZATION, request.getHeader(HttpHeaders.AUTHORIZATION));
+    }
+}
