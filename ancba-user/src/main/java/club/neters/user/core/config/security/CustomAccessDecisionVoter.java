8d08e578a192372313ab8d79fbcf33a3b34e5e97
anjoy8 <3143422472@qq.com>
2021-07-02 14:47:34 +0800

    feat：end security

--- ancba-user/src/main/java/club/neters/user/core/config/security/CustomAccessDecisionVoter.java
+++ ancba-user/src/main/java/club/neters/user/core/config/security/CustomAccessDecisionVoter.java
@@ -1,9 +1,12 @@
 package club.neters.user.core.config.security;
 
+import club.neters.user.core.constant.CommonConstant;
+import org.apache.commons.lang.ArrayUtils;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.access.AccessDecisionVoter;
 import org.springframework.security.access.ConfigAttribute;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.security.web.FilterInvocation;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.security.web.util.matcher.RequestMatcher;
@@ -30,15 +33,32 @@ public class CustomAccessDecisionVoter implements AccessDecisionVoter<FilterInvo
     @Override
     public int vote(Authentication authentication, FilterInvocation invocation, Collection<ConfigAttribute> attributes) {
         // TODO 扩展
-        String[] urls = {"/swagger-ui.html", "/swagger-resources/**",
-                "/webjars/**", "/v2/**","/test"};
+        String[] whiteList = CommonConstant.SECURITY_WHITELIST;
+        String[] permissionRequests = {"/test"};
         HttpServletRequest request = invocation.getRequest();
+        // OPTIONS全放开
         if (HttpMethod.OPTIONS.matches(request.getMethod())) {
             return ACCESS_GRANTED;
         }
-        for (String url : urls) {
+        // 白名单全放开
+        for (String url : whiteList) {
             RequestMatcher pathMatcher = new AntPathRequestMatcher(url);
             if (pathMatcher.matches(request)) {
+                return ACCESS_GRANTED;
+            }
+        }
+
+        // 开始处理授权验证逻辑
+        // TODO 此处可从数据库扩展处理，也可以使用任意其他的声明
+        String userNameVal = "";
+        try {
+            Jwt jwt = (Jwt) authentication.getPrincipal();
+            userNameVal = String.valueOf(jwt.getClaims().get("username"));
+        } catch (Exception ignored) {
+        }
+        for (String url : permissionRequests) {
+            RequestMatcher pathMatcher = new AntPathRequestMatcher(url);
+            if (pathMatcher.matches(request) && userNameVal.equals("test")) {
                 return ACCESS_GRANTED;
             }
         }
