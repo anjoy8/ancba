67dd7da87665a2428b2a89f93c33eb53c6c9ce59
anjoy8 <3143422472@qq.com>
2021-07-07 19:42:50 +0800

    Update CommonConstant.java

--- ancba-common/src/main/java/club/neters/common/constant/CommonConstant.java
+++ ancba-common/src/main/java/club/neters/common/constant/CommonConstant.java
@@ -7,5 +7,6 @@ public class CommonConstant {
     public static final String REDIS_SECURITY_ROLE_API_KEY = "security:roles_apis:";
     public static final String[] GW_SECURITY_WHITELIST = {"/", "/doc.html", "/oauth/**",
             "/*/v2/api-docs",
+            "/*/v2/api-docs-ext",
             "/actuator/**", "/error", "/open/api", "/actuator/health", "/actuator", "/swagger-ui.html", "/swagger-resources/**", "/webjars/**", "/v2/**", "/error"};
 }
