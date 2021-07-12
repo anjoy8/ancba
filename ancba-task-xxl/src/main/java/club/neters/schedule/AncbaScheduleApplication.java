6e0988fb801f242f3c3b56c9c352c61d994f5013
anjoy8 <3143422472@qq.com>
2021-07-12 20:12:48 +0800

    feat：tast task

---
+++ ancba-task-xxl/src/main/java/club/neters/schedule/AncbaScheduleApplication.java
@@ -0,0 +1,15 @@
+package club.neters.schedule;
+
+import org.springframework.boot.SpringApplication;
+
+/**
+ * Hello world!
+ *
+ */
+public class AncbaScheduleApplication
+{
+    public static void main(String[] args) {
+        SpringApplication.run(AncbaScheduleApplication.class, args);
+        System.out.println("================= task微服务启动成功 =============");
+    }
+}
