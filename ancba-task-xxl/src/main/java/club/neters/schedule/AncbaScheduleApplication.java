99a8177cc70bc95347ff106209237167590c08ca
anjoy8 <3143422472@qq.com>
2021-07-13 14:05:53 +0800

    feat：update xxl.job configure

--- ancba-task-xxl/src/main/java/club/neters/schedule/AncbaScheduleApplication.java
+++ ancba-task-xxl/src/main/java/club/neters/schedule/AncbaScheduleApplication.java
@@ -1,7 +1,6 @@
 package club.neters.schedule;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
--- ancba-task-xxl/src/main/java/club/neters/schedule/service/XxlJobService.java
+++ ancba-task-xxl/src/main/java/club/neters/schedule/service/XxlJobService.java
@@ -17,13 +17,11 @@ import java.util.concurrent.TimeUnit;
 
 /**
  * XxlJob开发示例（Bean模式）
- *
+ * <p>
  * 开发步骤：
  * 1、在Spring Bean实例中，开发Job方法，方式格式要求为 "public ReturnT<String> execute(String param)"
  * 2、为Job方法添加注解 "@XxlJob(value="自定义jobhandler名称", init = "JobHandler初始化方法", destroy = "JobHandler销毁方法")"，注解value值对应的是调度中心新建任务的JobHandler属性的值。
  * 3、执行日志：需要通过 "XxlJobLogger.log" 打印执行日志；
- *
- * @author xuxueli 2019-12-11 21:52:51
  */
 @Slf4j
 @Component
@@ -100,7 +98,7 @@ public class XxlJobService {
         if (exitValue == 0) {
             return IJobHandler.SUCCESS;
         } else {
-            return new ReturnT<String>(IJobHandler.FAIL.getCode(), "command exit value("+exitValue+") is failed");
+            return new ReturnT<String>(IJobHandler.FAIL.getCode(), "command exit value(" + exitValue + ") is failed");
         }
     }
 
@@ -178,11 +176,11 @@ public class XxlJobService {
         return ReturnT.SUCCESS;
     }
 
-    public void init(){
+    public void init() {
         log.info("init");
     }
 
-    public void destroy(){
+    public void destroy() {
         log.info("destory");
     }
 
